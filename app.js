a48a058e9c7ae71aee1e064337a40e31a0b51814
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-29 03:35:36 +0100

    update

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
-const express = require('express');
-const createError = require('http-errors');
-const morgan = require('morgan');
-const cors = require('cors');  // Import CORS middleware
-require('dotenv').config();
-const connectDB = require('./config/db'); // Import MongoDB connection
+const express = require("express");
+const createError = require("http-errors");
+const morgan = require("morgan");
+const cors = require("cors"); // Import CORS middleware
+require("dotenv").config();
+const connectDB = require("./config/db"); // Import MongoDB connection
 
-const { acknowledgmentEmailCron, weeklyFollowUpCron } = require('./crons');
+const { acknowledgmentEmailCron, weeklyFollowUpCron } = require("./crons");
 const app = express();
 
 // CORS Configuration
 const allowedOrigins = [
-  'http://localhost:5173', // Localhost (development)
-  'https://lease-car.vercel.app', // Production URL
+  "http://localhost:5173", // Localhost (development)
+  "https://lease-car.vercel.app", // Production URL
 ];
 
 // CORS options
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);  // Allow the origin or handle non-browser requests (like Postman)
+      callback(null, true); // Allow the origin or handle non-browser requests (like Postman)
     } else {
-      callback(new Error('Not allowed by CORS'), false);
+      callback(new Error("Not allowed by CORS"), false);
     }
   },
 };
 
 // Apply CORS middleware
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Localhost (development)
+      "https://lease-car.vercel.app", // Production URL
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
 
 // Connect to MongoDB
 connectDB();
-app.get('/', async (req, res, next) => {
-  res.send({ message: 'Awesome it works 🐻' });
+app.get("/", async (req, res, next) => {
+  res.send({ message: "Awesome it works 🐻" });
 });
 
-app.use('/api/email', require('./routes/api.route'));
+app.use("/api/email", require("./routes/api.route"));
 
 // Start cron jobs
 acknowledgmentEmailCron();
